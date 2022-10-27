b8a5d8b70a3869aa57e31c995d3e39e6ad56f49f
Мария Ларичева <43138491+MariaLaricheva@users.noreply.github.com>
2022-10-27 16:34:47 +0300

    fixed z-index

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,12 +136,12 @@ var swiperTestimonial = new Swiper(".testimonial__container", {
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
-    const scrollY = window.pageYOffset
+    const scrollY = window.scrollY
 
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        let sectionId = current.getAttribute('id')
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
@@ -163,12 +163,17 @@ function scrollHeader() {
 window.addEventListener('scroll', scrollHeader)
 
 /*=== TESTIMONIAL ==*/
-
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    const menu = document.getElementById('nav-menu')
+    if (this.scrollY >= 560) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll')
+    }
+
 }
 
 window.addEventListener('scroll', scrollUp)
